5fbfece12dfa3456d20216f32c54070637094ebf
billnguyen93 <105510509+billnguyen93@users.noreply.github.com>
2022-06-22 02:17:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 // Write your solution in this file!
 const employee = {
-    name: "Max",
-    streetAddress: "5 Main Street",
-  };
-  
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
-    // Alternate using ES6 Spread operators:
+    name: 'Ben',
+    streetAddress: '6 North Street'
+};
 
-    return {...employee, [key]: value};
+function updateEmployeeWithKeyAndValue(){
+    const updatedEmployee = {...employee};
+    updatedEmployee.name= "Sam";
+    updatedEmployee.streetAddress = "11 Broadway";
+    return updatedEmployee;
+
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue (obj, key, value){
+    obj [key] = value;
+    return obj;
+};
+
+function deleteFromEmployeeByKey (obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+    delete obj[key];
     
-  }
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+};
